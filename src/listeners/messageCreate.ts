5850441ea092f54fd056543a663588af89175197
thehairy <soeren.stabenow@outlook.com>
2022-11-30 22:49:31 +0100

    feat: iec listener automation

--- src/listeners/messageCreate.ts
+++ src/listeners/messageCreate.ts
@@ -3,6 +3,7 @@ import { Listener } from '@sapphire/framework';
 import type { Message } from "discord.js";
 import { envParseArray } from "../lib/env-parser";
 import { sendLogMessage } from "../lib/utils";
+import type { ThreadChannel } from "discord.js";
 
 @ApplyOptions<Listener.Options>({ once: false })
 export class MessageEvent extends Listener {
@@ -15,7 +16,7 @@ export class MessageEvent extends Listener {
     }
 
     public async run(message: Message) {
-        if (message.author.bot || message.member?.permissions.has('ADMINISTRATOR')) return;
+        if (message.author.bot) return;
         if (!message.client.reportedMessages) message.client.reportedMessages = [];
 
         const { logger } = this.container;
@@ -23,10 +24,17 @@ export class MessageEvent extends Listener {
         const arrayForbiddenEmojis = forbiddenEmojis.filter(e => message.content.includes(e));
 
         // If there is a forbidden emoji in the message
-        if (arrayForbiddenEmojis.length > 0) {
+        if (arrayForbiddenEmojis.length > 0 && !message.member?.permissions.has('ADMINISTRATOR')) {
             logger.info(`Received a message with a forbidden emoji from ${message.author.tag}: ${message.content}`);
             const deleted = await message.delete().then(() => true).catch(() => false);
             await sendLogMessage(logger, message.url, message.channel.toString(), message.member!, message.client, deleted, arrayForbiddenEmojis)
+        } else {
+            // 11-bit studios specific:
+            if (message.channel.id === '1000374509456593046' && !message.member?.roles.cache.has('973966310872596521')) {
+                await message.member?.roles.add('973966310872596521').catch(() => {});
+                await message.reply('Role added :)');
+                await (message.channel as ThreadChannel).members.remove(message.author.id);
+            }
         }
     }
 }
